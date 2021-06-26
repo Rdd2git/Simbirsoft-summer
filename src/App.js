412035b3d738e31ef097c5b4fed2713468c04299
Radik <rgaliullin55@gmail.com>
2021-06-26 11:14:36 +0300

    add leagues page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import LeaguesPage from './components/pages/leagues'
 
 function App() {
   return (
     <div className="App">
-      
+      <LeaguesPage />
     </div>
   );
 }
---
+++ src/components/pages/leagues/leagues.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Leagues() {
+  return (
+    <div>
+      leagues page
+    </div>
+  );
+}
+
+export default Leagues;
